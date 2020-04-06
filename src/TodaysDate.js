ccc42a43fc2792308825131aed87a246b485bde8
Pankaj Yadav <pankaj.azm2@gmail.com>
2020-04-06 19:10:54 +0530

    Myname repo added

--- src/TodaysDate.js
+++ src/TodaysDate.js
@@ -1,5 +1,6 @@
 import React from "react";
+import { Myname } from 'myname-react-component-example';
 
-const TodaysDate = () => <div>{`Today's date is ${Date()}`}</div>;
+const TodaysDate = () => <div>{`Today's date is ${Date()}`}<Myname name="Pankaj" /></div>;
 
 export default TodaysDate;
